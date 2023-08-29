a41a833ed65f75bdcba40ac837f81e3b3d961562
Eziz Rehimov <fantastig@bk.ru>
2023-08-29 13:05:41 +0400

    Task finished

---
+++ 26AugClass/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _26AugClass
+{
+    class Product
+
+    {
+        private static int IdChange = 0;
+
+
+        public int Id;
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int Income = 0;
+        public int Count = 0;
+
+        public Product(string brandName, string model, int price, int count, int income, int cost)
+        {
+            IdChange++;
+            Id = IdChange;
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            Income = income;
+            Count = count;
+            Sale();
+
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Id}, {BrandName}, {Model}, {Price}, {Cost}, {Income}, {Count}");
+        }
+
+        public void Sale()
+        {
+            if (Count > 0)
+            {
+                int value = Cost - Price;
+                Income += value;
+                Count--;
+            }
+        }
+    }
+}
---
+++ 26AugClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26AugClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product("BMW", "X5", 300, 5, 100, 200);
+            Product product1 = new Product("BMW", "X6", 300, 5, 100, 200);
+
+            product.GetInfo();
+            product1.GetInfo();
+
+
+
+        }
+    }
+}
